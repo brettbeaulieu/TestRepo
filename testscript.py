ba0314eaf129bb7770d4945c75c742296e783d83
Brett Beaulieu <user@example.com>
2025-01-19 11:12:01 -0500

    SBT-18 things

--- testscript.py
+++ testscript.py
@@ -9,6 +9,7 @@ def twoDiceRoll():
         result.append(random.randint(1, 6))
 
     rolls_sum = sum(result)
+    print("Testing for SBT-17 (Caps)")
     print("Two Dice Roll Summary: ")
     print(f'Rolls: {result}')
     print(f'Sum: {rolls_sum}')
