d38f7ce1fb93d0ffd6daeb9bb3e3fb190625159f
Brett Beaulieu <user@example.com>
2025-01-19 08:39:58 -0500

    Addressing SB-14, added a detailed printout to the dice roll function

--- testscript.py
+++ testscript.py
@@ -8,7 +8,11 @@ def main():
     for roll in range(total_rolls):
         result.append(random.randint(1, 6))
 
-    print(sum(result))
+    rolls_sum = sum(result)
+
+    print(f'Rolls: {result}')
+    print(f'Sum: {rolls_sum}')
+    print(f'Average: {rolls_sum / total_rolls}')
 
 
 if __name__ == "__main__":
